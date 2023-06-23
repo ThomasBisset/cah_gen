76ef125d0bbf610d5702af8ae9139255314bb255
Thomas Bisset <5402845+ThomasBisset@users.noreply.github.com>
2023-06-24 00:22:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# cah_gen 
+
+## Huh?
+
+Inspired by the old Unix program `fortune`, a terminal application to randomly select a black and white card pairing 
+from the game Cards Against Humanity, implemented in Python.
+
+## Why?
+
+Because I can.
+
+## Who?
+
+I made this. But I did get the text files with the cards from [Chris Hallberg](https://crhallberg.com/cah/); thanks!
+
+Cards Against Humanity released the game under a Creative Commons License, which means I can legally make this as well, 
+so thanks for that.
---
+++ main.py
@@ -0,0 +1,31 @@
+# import libraries
+import random
+import re
+
+# read text files
+black_file = open("black.txt", "r")
+white_file = open("white.txt", "r")
+
+# convert text files to Python lists
+all_black_cards = black_file.read().split("\n")
+all_white_cards = white_file.read().split("\n")
+
+# pick a random black card
+black_card = random.choice(all_black_cards)
+print(black_card)
+
+# search black card for number of blanks (underscores) and it's location in the string
+blank_spaces = []
+for underscore in re.finditer("_", black_card):
+    blank_spaces = blank_spaces + [underscore.start()]
+
+# pick n white cards depending on number of blanks above
+white_card = []
+if len(blank_spaces) < 1:
+    white_card = white_card + [random.choice(all_white_cards)]
+else:
+    for _i in blank_spaces:
+        white_card = white_card + [random.choice(all_white_cards)]
+print(white_card)
+
+# combine into a string to output
