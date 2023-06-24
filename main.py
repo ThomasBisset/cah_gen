7029ea8bb989d0b3518e53bad3ca47163d794a33
Thomas Bisset <5402845+ThomasBisset@users.noreply.github.com>
2023-06-24 13:27:37 +0100

    Changes and improvements

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ white_file = open("white.txt", "r")
 all_black_cards = black_file.read().split("\n")
 all_white_cards = white_file.read().split("\n")
 
+# remove full stops from end of all white cards
+
+
 # pick a random black card
 black_card = random.choice(all_black_cards)
-print(black_card)
 
 # search black card for number of blanks (underscores) and it's location in the string
 blank_spaces = []
@@ -22,10 +24,22 @@ for underscore in re.finditer("_", black_card):
 # pick n white cards depending on number of blanks above
 white_card = []
 if len(blank_spaces) < 1:
-    white_card = white_card + [random.choice(all_white_cards)]
+    print(black_card, random.choice(all_white_cards))
 else:
     for _i in blank_spaces:
         white_card = white_card + [random.choice(all_white_cards)]
-print(white_card)
 
 # combine into a string to output
+if len(blank_spaces) >= 1:
+    black_card = black_card.replace("_", "{}")
+    print(black_card.format(*white_card))
+
+# debug shit
+print("----")
+print(black_card)
+print(white_card)
+
+# TODO: Haiku specific case
+# TODO: _[SAME CARD]_ specific case
+# TODO: Punctuation and capitalisation correction
+# TODO: Respect newlines
