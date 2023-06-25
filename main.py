7c4820e7c9b1d35bfce60102f7f5eb16ba124af5
Thomas Bisset <5402845+ThomasBisset@users.noreply.github.com>
2023-06-25 22:17:03 +0100

    It actually works!

--- main.py
+++ main.py
@@ -1,45 +1,50 @@
 # import libraries
 import random
-import re
+
+
+def pick_random_card(file, n):
+    stack = file.read().split("\n")
+    pick = []
+    for _loop in range(n):
+        pick.append(random.choice(stack))
+    return pick
+
 
 # read text files
-black_file = open("black.txt", "r")
-white_file = open("white.txt", "r")
-
-# convert text files to Python lists
-all_black_cards = black_file.read().split("\n")
-all_white_cards = white_file.read().split("\n")
-
-# remove full stops from end of all white cards
+black_stack = open("black.txt", "r")
+white_stack = open("white.txt", "r")
 
 
-# pick a random black card
-black_card = random.choice(all_black_cards)
+# draw a black card
+black_pick = pick_random_card(black_stack, 1)[0]
 
-# search black card for number of blanks (underscores) and it's location in the string
-blank_spaces = []
-for underscore in re.finditer("_", black_card):
-    blank_spaces = blank_spaces + [underscore.start()]
 
-# pick n white cards depending on number of blanks above
-white_card = []
-if len(blank_spaces) < 1:
-    print(black_card, random.choice(all_white_cards))
-else:
-    for _i in blank_spaces:
-        white_card = white_card + [random.choice(all_white_cards)]
+flag = black_pick.count("_")
+if "SAME CARD AGAIN" in black_pick:
+    flag = "same"
+if "Make a haiku." in black_pick:
+    flag = "haiku"
 
-# combine into a string to output
-if len(blank_spaces) >= 1:
-    black_card = black_card.replace("_", "{}")
-    print(black_card.format(*white_card))
 
-# debug shit
-print("----")
-print(black_card)
-print(white_card)
+if flag == "haiku":
+    print(black_pick)
+    print("[", pick_random_card(white_stack, 3), "]")
+elif flag == "same":
+    white_pick = pick_random_card(white_stack, 1)
+    black_pick = black_pick.replace("_[SAME CARD AGAIN]_", "[{}]")
+    black_pick = black_pick.replace("_(SAME CARD AGAIN)_", "[{}]")
+    black_pick = black_pick.replace("_", "{}")
+    print(black_pick.format(*white_pick, *white_pick))
+elif flag == 0:
+    print(black_pick)
+    print("[" + str(*pick_random_card(white_stack, 1)) + "]")
+elif flag > 0:
+    black_pick = black_pick.replace("_", "[{}]")
+    white_pick = pick_random_card(white_stack, flag)
+    white_pick = [i.rstrip(".") for i in white_pick]
+    print(black_pick.format(*white_pick))
 
-# TODO: Haiku specific case
-# TODO: _[SAME CARD]_ specific case
-# TODO: Punctuation and capitalisation correction
+
+# TODO: Capitalisation correction
 # TODO: Respect newlines
+# TODO: Colours?
