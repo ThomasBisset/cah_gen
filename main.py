f4ab250c5a01f02f2a8a229db54606b3d1c7532c
Thomas Bisset <5402845+ThomasBisset@users.noreply.github.com>
2023-06-25 23:51:29 +0100

    removed some unneccessary code

--- main.py
+++ main.py
@@ -20,31 +20,24 @@ black_pick = pick_random_card(black_stack, 1)[0]
 black_pick = black_pick.replace("\\n", "")
 
 
-flag = black_pick.count("_")
-if "SAME CARD AGAIN" in black_pick:
-    flag = "same"
 if "Make a haiku." in black_pick:
-    flag = "haiku"
-
-
-if flag == "haiku":
     print(black_pick)
     white_pick = pick_random_card(white_stack, 3)
     for i in white_pick:
         print("[" + i + "]")
-elif flag == "same":
+elif "SAME CARD AGAIN" in black_pick:
     white_pick = pick_random_card(white_stack, 1)
     white_pick = [i.rstrip(".") for i in white_pick]
     black_pick = black_pick.replace("_[SAME CARD AGAIN]_", "[{}]")
     black_pick = black_pick.replace("_(SAME CARD AGAIN)_", "[{}]")
     black_pick = black_pick.replace("_", "[{}]")
     print(black_pick.format(*white_pick, *white_pick))
-elif flag == 0:
+elif black_pick.count("_") == 0:
     white_pick = "[" + str(*pick_random_card(white_stack, 1)) + "]"
     print(black_pick)
     print(white_pick)
-elif flag > 0:
-    white_pick = pick_random_card(white_stack, flag)
+else:
+    white_pick = pick_random_card(white_stack, black_pick.count("_"))
     white_pick = [i.rstrip(".") for i in white_pick]
     white_pick = [i.replace("\\n", "") for i in white_pick]
     black_pick = black_pick.replace("_", "[{}]")
