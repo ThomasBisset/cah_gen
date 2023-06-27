a28230977e5302867ecd6d1f7cc5d27a3010581c
Thomas Bisset <5402845+ThomasBisset@users.noreply.github.com>
2023-06-27 21:16:47 +0100

    It works much better!

--- cookie-cah.py
+++ cookie-cah.py
@@ -3,30 +3,29 @@ import random
 import json
 
 
-def random_card(deck, colour, n):
-    output = []
-    for _i in range(n):
-        n_sets = len(deck)
-        pack = random.randint(0, n_sets)
-        output.append(random.choice(deck[pack][colour]))
-    return output
-
-
 # load json
-with open("cah-cards-full.json") as json_file:
+with open("cah-cards-compact.json") as json_file:
     full_deck = json.load(json_file)
 
 
 # pick random black card
-black = random_card(full_deck, "black", 1)[0]
-black_output = black["text"].replace("_", "[{}]")
+black = random.choice(full_deck["black"])
+black["text"] = black["text"].replace("_(SAME CARD AGAIN)_", "[{}]")
+black["text"] = black["text"].replace("_[SAME CARD AGAIN]_", "[{}]")
+black["text"] = black["text"].replace("_", "[{}]")
 
 
 # pick random white card(s)
-white = random_card(full_deck, "white", black["pick"])
-white_output = []
-for i in white:
-    white_output = i["text"].rstrip(".")
+white = []
+for i in range(black["pick"]):
+    white.append(random.choice(full_deck["white"]).rstrip("."))
 
-# output
-print(black_output.format(white_output))
+
+# print output strings
+if black["text"].find("[{}]") == -1:
+    print(black["text"])
+    for i in white:
+        print("[", end="")
+        print(i, end=".]\n")
+else:
+    print(black["text"].format(*white))
