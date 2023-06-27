a4a7084449befc41f4713297bb7a4de26bc83161
Thomas Bisset <5402845+ThomasBisset@users.noreply.github.com>
2023-06-27 09:42:31 +0100

    test random.choice method

--- cookie-cah.py
+++ cookie-cah.py
@@ -6,17 +6,27 @@ import json
 def random_card(deck, colour, n):
     output = []
     for _i in range(n):
-        n_sets = len(deck) - 1
+        n_sets = len(deck)
         pack = random.randint(0, n_sets)
-        output.append(random.choice(deck[pack][colour])["text"])
+        output.append(random.choice(deck[pack][colour]))
     return output
 
 
+# load json
 with open("cah-cards-full.json") as json_file:
     full_deck = json.load(json_file)
 
 
-# format = cards[index of set][card colour][card index][text, pack kvp]
+# pick random black card
+black = random_card(full_deck, "black", 1)[0]
+black_output = black["text"].replace("_", "[{}]")
 
 
-print(random_card(full_deck, "white", 3))
+# pick random white card(s)
+white = random_card(full_deck, "white", black["pick"])
+white_output = []
+for i in white:
+    white_output = i["text"].rstrip(".")
+
+# output
+print(black_output.format(white_output))
